9f2ad923352e4c4de814613a49001fc2532b4fda
t8230089 <t8230089@aueb.gr>
2024-11-13 17:12:52 +0200

    Update Team9.java

--- Team9.java
+++ Team9.java
@@ -1,9 +1,7 @@
 public class Team9 {
     public static void main(String[] args) {
         System.out.println("8230089");
-
         System.out.println("8230031");
-
         System.out.println(8230134);
         System.out.println("8230025");
      }
